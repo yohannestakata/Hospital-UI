3036a06de57388f18bd5d4e69d3a86709e6d6c1b
Yohannes Takata <johntakata002@gmail.com>
2024-02-21 21:39:07 +0300

    updated ultrasound waitinglist for external orders

--- src/features/ultrasound/UltrasoundWaitlist.jsx
+++ src/features/ultrasound/UltrasoundWaitlist.jsx
@@ -23,33 +23,64 @@ function UltrasoundWaitlist() {
         </thead>
         <tbody>
           {waitlist?.map((item) => {
-            return (
-              <tr
-                onClick={() => {
-                  navigate(
-                    `/ultrasound/${item.orderId.type}?patientId=${item.patientId._id}&doctorId=${item.doctorId._id}&orderId=${item.orderId._id}&patientName=${item.patientId.name}&queueId=${item.queueId}&doctorName=${item.doctorId.name}&waitingId=${item._id}&xrayTechniques=${item.orderId.xrayTechniques}`,
-                  );
-                }}
-                className="cursor-pointer border-b bg-white hover:bg-black/10"
-                key={item._id}
-              >
-                <td className="px-6 py-4 font-amharic font-bold text-slate-700">
-                  {item.patientId.name}
-                </td>
-                <td className="px-6 py-4">{item.doctorId.name}</td>
-                <td className="px-6 py-4">
-                  <span
-                    className={`${
-                      item.orderId.type === "Ultrasound"
-                        ? "bg-violet-800 text-violet-50"
-                        : "bg-rose-800 text-rose-50"
-                    } rounded-full px-2 `}
-                  >
-                    {item.orderId.type}
-                  </span>
-                </td>
-              </tr>
-            );
+            console.log(item);
+            if (!item.isExternal)
+              return (
+                <tr
+                  onClick={() => {
+                    navigate(
+                      `/ultrasound/${item.orderId.type}?patientId=${item.patientId._id}&doctorId=${item.doctorId._id}&orderId=${item.orderId._id}&patientName=${item.patientId.name}&queueId=${item.queueId}&doctorName=${item.doctorId.name}&waitingId=${item._id}&xrayTechniques=${item.orderId.xrayTechniques}`,
+                    );
+                  }}
+                  className="cursor-pointer border-b bg-white hover:bg-black/10"
+                  key={item._id}
+                >
+                  <td className="px-6 py-4 font-amharic font-bold text-slate-700">
+                    {item.patientId.name}
+                  </td>
+                  <td className="px-6 py-4">{item.doctorId.name}</td>
+                  <td className="px-6 py-4">
+                    <span
+                      className={`${
+                        item.orderId.type === "Ultrasound"
+                          ? "bg-violet-800 text-violet-50"
+                          : "bg-rose-800 text-rose-50"
+                      } rounded-full px-2 `}
+                    >
+                      {item.orderId.type}
+                    </span>
+                  </td>
+                </tr>
+              );
+            else {
+              return (
+                <tr
+                  onClick={() => {
+                    navigate(
+                      `/ultrasound/${item.orderId.type}?patientId=${item.patientId._id}&doctorId=${item.doctorId._id}&orderId=${item.orderId._id}&patientName=${item.patientId.name}&queueId=${item.queueId}&doctorName=${item.doctorId.name}&waitingId=${item._id}&xrayTechniques=${item.orderId.xrayTechniques}`,
+                    );
+                  }}
+                  className="cursor-pointer border-b bg-white hover:bg-black/10"
+                  key={item._id}
+                >
+                  <td className="px-6 py-4 font-amharic font-bold text-slate-700">
+                    {item.bid.name}
+                  </td>
+                  <td className="px-6 py-4">External</td>
+                  <td className="px-6 py-4">
+                    <span
+                      className={`${
+                        item.orderId.type === "Ultrasound"
+                          ? "bg-violet-800 text-violet-50"
+                          : "bg-rose-800 text-rose-50"
+                      } rounded-full px-2 `}
+                    >
+                      {item.orderId.type}
+                    </span>
+                  </td>
+                </tr>
+              );
+            }
           })}
         </tbody>
       </table>
