bfa250b1b98fc719aca50b8a7043ea4e75083daf
Yohannes Takata <johntakata002@gmail.com>
2024-02-21 23:45:53 +0300

    Fixed xray external support

--- src/features/xray/XrayWriteTest.jsx
+++ src/features/xray/XrayWriteTest.jsx
@@ -3,6 +3,8 @@ import useGetPatient from "../../hooks/useGetPatient";
 import { useState } from "react";
 import useAddXrayResult from "./useAddXrayResult";
 import useGetXrayOrder from "./useGetXrayOrder";
+import PrintTemplate from "../../components/PrintTemplate";
+import { FaPrint } from "react-icons/fa";
 
 function formatDate(date) {
   return new Date(date).toLocaleDateString([], {
@@ -30,6 +32,11 @@ function XrayWriteTest() {
   const [techniquesText, setTechniquesText] = useState("");
 
   console.log(order);
+  const { ipcRenderer } = window.require("electron");
+
+  const handlePrint = () => {
+    ipcRenderer.send("print");
+  };
 
   const { mutate: addResult } = useAddXrayResult();
 
@@ -76,20 +83,26 @@ function XrayWriteTest() {
                 <td>Name:</td>
                 <td className="px-2 ">
                   <span className="font-amharic font-bold">
-                    {patient?.name}
+                    {isExternal ? order?.bid.name : patient?.name}
                   </span>
                 </td>
               </tr>
               <tr>
                 <td>Age:</td>
                 <td className="px-2 ">
-                  <span className="font-bold"> {patient?.age}</span>
+                  <span className="font-bold">
+                    {" "}
+                    {isExternal ? order?.bid.age : patient?.age}
+                  </span>
                 </td>
               </tr>
               <tr>
                 <td>Sex:</td>
                 <td className="px-2 ">
-                  <span className="font-bold"> {patient?.gender}</span>
+                  <span className="font-bold">
+                    {" "}
+                    {isExternal ? order?.bid.sex : patient?.gender}
+                  </span>
                 </td>
               </tr>
               <tr>
@@ -107,50 +120,92 @@ function XrayWriteTest() {
             </tbody>
           </table>
         </div>
-        <div className="mt-6">
-          <h2 className="text-lg font-bold">Xray Order</h2>
-          <p className="mt-2">{order?.orderedTest}</p>
-        </div>
-        <div className="mt-6">
-          <form action="" className="" onSubmit={handleSubmit}>
-            {!isExternal && (
-              <label htmlFor="techniques" className="flex flex-col gap-1">
-                {order?.orderedTest.split(", ").map((test) => {
-                  return (
-                    <div>
-                      <span className="ml-1 mt-2 text-sm font-bold">
-                        {test} Techniques
-                      </span>
-                      <textarea
-                        value={techniquesText[test]}
-                        onChange={(e) =>
-                          setTechniquesText((prev) => ({
-                            ...prev,
-                            [test]: e.target.value,
-                          }))
-                        }
-                        name=""
-                        id="techniques"
-                        cols="30"
-                        rows="4"
-                        placeholder="Techniques"
-                        className="w-full rounded-md border-2 border-gray-500 px-3 py-1 focus:outline-blue-800"
-                      ></textarea>
-                    </div>
-                  );
-                })}
-              </label>
-            )}
-            {isExternal && order?.externalOrder}
-
-            <button
-              type="submit"
-              className="ml-auto mt-2 block rounded-md bg-green-800 px-4 py-2 text-green-50 duration-100 hover:bg-green-900"
-            >
-              Submit
-            </button>
-          </form>
-        </div>
+        <PrintTemplate
+          patient={{
+            name: order?.bid.name,
+            age: order?.bid.age,
+            gender: order?.bid.sex,
+            cardNumber: "External",
+          }}
+        >
+          <div className="mt-6">
+            <h2 className="text-lg font-bold">Xray Order</h2>
+            <p className="mt-2">
+              {isExternal ? order?.externalOrder : order?.orderedTest}
+            </p>
+          </div>
+          <div className="mt-6">
+            <form action="" className="" onSubmit={handleSubmit}>
+              {!isExternal && (
+                <label htmlFor="techniques" className="flex flex-col gap-1">
+                  {order?.orderedTest.split(", ").map((test) => {
+                    return (
+                      <div>
+                        <span className="ml-1 mt-2 text-sm font-bold">
+                          {test} Techniques
+                        </span>
+                        <textarea
+                          value={techniquesText[test]}
+                          onChange={(e) =>
+                            setTechniquesText((prev) => ({
+                              ...prev,
+                              [test]: e.target.value,
+                            }))
+                          }
+                          name=""
+                          id="techniques"
+                          cols="30"
+                          rows="4"
+                          placeholder="Techniques"
+                          className="w-full rounded-md border-2 border-gray-500 px-3 py-1 focus:outline-blue-800"
+                        ></textarea>
+                      </div>
+                    );
+                  })}
+                </label>
+              )}
+              {isExternal && (
+                <label htmlFor="techniques" className="flex flex-col gap-1">
+                  <div>
+                    <span className="ml-1 mt-2 text-sm font-bold">
+                      Techniques
+                    </span>
+                    <textarea
+                      value={techniquesText["tech"]}
+                      onChange={(e) =>
+                        setTechniquesText((prev) => ({
+                          ...prev,
+                          ["tech"]: e.target.value,
+                        }))
+                      }
+                      name=""
+                      id="techniques"
+                      cols="30"
+                      rows="4"
+                      placeholder="Techniques"
+                      className="w-full rounded-md border-2 border-gray-500 px-3 py-1 focus:outline-blue-800"
+                    ></textarea>
+                  </div>
+                </label>
+              )}
+              <div className="flex justify-end gap-2 print:hidden">
+                <button
+                  onClick={handlePrint}
+                  className="mt-2 flex items-center gap-1 rounded-md border border-black/50 px-4 py-2 duration-100 hover:bg-green-900 hover:text-green-200"
+                >
+                  <FaPrint />
+                  Print
+                </button>
+                <button
+                  type="submit"
+                  className=" mt-2 block rounded-md bg-green-800 px-4 py-2 text-green-50 duration-100 hover:bg-green-900"
+                >
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </PrintTemplate>
       </div>
     </div>
   );
